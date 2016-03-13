99ae2804eca976021df0a9307bb6409b36d66f0c
SilvaHaberl <silva.haberl1989@gmail.com>
2016-03-13 18:29:58 +0100

    promjena

--- functions.php
+++ functions.php
@@ -1,40 +1,30 @@
 <?php
 
-
-	
+	// Povezivanje s bazom podataka
 	$connect = mysql_connect("localhost","root","");
 	mysql_select_db("test_db",$connect);
-      
-
+	if($connect === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+	}
+    // GET metoda slanja podataka novog zadatka
     $otvaranje = $_GET["Otvaranje"];
     $naslov = $_GET["Naslov"];
     $opis = $_GET["Opis"];
 	$status = $_GET["Status"];
 	$zavrsetak = $_GET["Zavrsetak"];
 	
-
-    
-
- /*
-	$sql1 = mysql_query("SELECT status FROM zadaci WHERE naslov = '$naslov' ",$sql_connection);
-
+	// Pretraga tablice 'zadaci' o zadacima koji osim istog naslova imaju i isti status
+	$sql1 = mysql_query("SELECT status FROM zadaci WHERE naslov = '$naslov' ",$connect);
+	$response1= mysql_fetch_array($sql1, MYSQL_NUM);
+	if($response1[0] == $status){ 
+		error_log("Error! Postoji vec zadatak s tim naslovom i statusom.",0); 
+		echo "Upisano u error_log";
+	} 
 	
-    $response1= mysql_fetch_array($sql1, MYSQL_NUM);
-	echo $response1[0];
-	$sql2 = mysql_query("SELECT naslov FROM zadaci WHERE status = $status ");
-	
-
-	if(($sql1!=null)&&($response1==$status)&&($response1!=null)){ 
-	
-	error_log("Error! Postoji vec zadatak s tim naslovom i statusom.",0); 
-	echo "Upisano u error_log";
-	} */
- 
+	// Unos podataka poslanih GET metodom u tablicu 'zadaci' baze 'test_db'
 	$sql = "INSERT INTO zadaci (otvaranje, naslov, opis, status, zavrsetak)
 	VALUES ('$otvaranje', '$naslov', '$opis', '$status', '$zavrsetak')";
-
 	mysql_query($sql, $connect);
- 
     mysql_close($connect);
 	  
 	  
--- index.php
+++ index.php
@@ -1,53 +1,63 @@
 <!doctype html>
 <html lang="en">
+
 <head>
- 
 <link href='http://fonts.googleapis.com/css?family=Spicy+Rice' rel='stylesheet' type='text/css'>
 <title>Lista zadataka</title>
 </head>
+
 <body>
 
 <?php
 
-
+ // Povezivanje s bazom podataka
  $conn = mysql_connect("localhost","root","");
  mysql_select_db("test_db");
- /*
-   if(! $conn ) {
-    die('Could not connect: ' . mysql_error());
-    }
+ if(! $conn ) {
+    
+ die('Ne mogu se povezati s bazom: ' . mysql_error());
+ 
+ }
 
-    echo 'Connected successfully';
-
-	  $sql = 'CREATE TABLE zadaci( '.
-	'ID INT, '.
-    'otvaranje DATETIME NOT NULL , '.
+ echo "Uspjesno povezivanje s bazom.";
+ 
+ 
+ // Provjera je li baza kreirana, kreiraj
+ $val = mysql_query('select 1 from `zadaci` LIMIT 1');
+ if($val == FALSE)
+{
+	$sql = 'CREATE TABLE zadaci( '.
+	'otvaranje DATETIME NOT NULL , '.
     'naslov VARCHAR(20) NOT NULL, '.
     'opis  TEXT NOT NULL, '.
-    'status   VARCHAR(20) NOT NULL, '.
-    'zavrsetak    DATE NOT NULL, '.
-    'primary key (ID ))';
-
-
-   
-    $retval = mysql_query( $sql, $conn );
-
-    if(! $retval ) {
-    die('Could not create table: ' . mysql_error());
-    }
-
-    echo "Table zadaci created successfully\n";
-
-    mysql_close($conn);
-	*/
-
-	$query=mysql_query("SELECT * FROM zadaci ");	
-	while ($row = mysql_fetch_array($query, MYSQL_NUM)) {
-    print "<div>$row[1] $row[2] $row[3] $row[4] $row[5]</div>\n";}
-	mysql_free_result($query);
+    'status   VARCHAR(20) , '.
+    'zavrsetak    DATE , '.
+    'primary key (naslov ))';
 
 	
-	?>
+	$retval = mysql_query( $sql, $conn );
+
+	
+	if(!$retval ) {
+    die('Nije moguce kreirati tablicu: ' . mysql_error());
+    }
+	echo "Tablica 'zadaci' je kreirana.\n";
+		
+}
+ else
+{
+    echo 'Tablica "zadaci" vec postoji.';
+}
+	
+	
+	//ispis podataka iz tablice 'zadaci'
+	
+	$query=mysql_query("SELECT * FROM zadaci ");
+	while ($row = mysql_fetch_array($query, MYSQL_NUM)) {
+    print "<div>$row[1] $row[2] $row[3] $row[4] </div>\n";} 
+	mysql_free_result($query);
+?>
+	
 	<form action="functions.php" method="get">
         <table>
             <tr>
